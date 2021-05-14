77ceb1e6dcbb963d3bc194968345f6a567e26c91
Black devil h4 <79358591+abhijithmanu@users.noreply.github.com>
2021-05-14 09:41:28 +0530

    Update Android-hacking. py

--- Android-Hacking.py
+++ Android-Hacking.py
@@ -4,3 +4,9 @@ import time
 import say
 def load (word):
          for char in word:
+                print(char, end =")
+                says. stdout. flush()
+                time. sleep(0.2)
+def bash():
+          print("\033[31m")
+          print("
