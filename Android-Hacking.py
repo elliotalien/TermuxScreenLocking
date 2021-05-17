e4f882da6ebc01d18ce027b06cdc998ca5168fbc
Black devil h4 <79358591+abhijithmanu@users.noreply.github.com>
2021-05-17 07:35:30 +0530

    Update Android-Hacking. py

--- Android-Hacking.py
+++ Android-Hacking.py
@@ -100,7 +100,7 @@ print(" [4] Man-in-the-middle attack "\n\n")
                         
                         time.sleep(3)
                         os.system("cmatrix-L")
-                     p
+                     
 
             
       
