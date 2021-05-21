4ee282266105edbeb6daffbd740d40f39fcbf85f
Black devil h4 <79358591+abhijithmanu@users.noreply.github.com>
2021-05-21 07:40:22 +0530

    Update Android-Hacking.py

--- Android-Hacking.py
+++ Android-Hacking.py
@@ -1,8 +1,8 @@
 #!bin/python
 import os
 import time
-import say
-def load (word):
+ import say
+ def load (word):
          for char in word:
                 print( char,end='')
                 says.stdout.flush()
